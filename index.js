d1c1f9ff9d48799d0b23d2c18208fa0d6ce307f0
Anton <ushkov6@mail.ru>
2023-04-28 01:28:05 +0400

    add timer functional

--- index.js
+++ index.js
@@ -2,10 +2,46 @@ const inputEl = document.querySelector("input");
 const buttonEl = document.querySelector("button");
 const timerEl = document.querySelector("span");
 
+let timeInterval = null;
+
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return seconds => {};
+  const setZeroBefore = value => {
+    if (value < 10) {
+      return "0" + value;
+    }
+    return value;
+  };
+
+  const setFormatTime = seconds => {
+    let getSeconds = setZeroBefore(seconds % 60),
+      getHours = setZeroBefore(Math.floor(seconds / 60 / 60)),
+      getMinutes = setZeroBefore(Math.floor(seconds / 60) - getHours * 60);
+
+    return `${getHours}:${getMinutes}:${getSeconds}`;
+  };
+
+  return seconds => {
+    let passedTime = 0,
+      currentTime = seconds;
+
+    const startFormatFunc = () => {
+      timerEl.innerHTML = setFormatTime(currentTime);
+    };
+
+    startFormatFunc();
+    timeInterval = setInterval(() => {
+      passedTime = passedTime += 1;
+      currentTime = seconds - passedTime;
+
+      if (passedTime !== seconds + 1) {
+        startFormatFunc();
+      } else {
+        clearInterval(timeInterval);
+      }
+    }, 1000);
+  };
 };
 
 const animateTimer = createTimerAnimator();
@@ -21,6 +57,10 @@ inputEl.addEventListener("input", e => {
 
 buttonEl.addEventListener("click", () => {
   const seconds = Number(inputEl.value);
+
+  if (timeInterval) {
+    clearInterval(timeInterval);
+  }
 
   animateTimer(seconds);
 
