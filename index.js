62ca498ff7adf81e2fd3d7dfed8e2338e3613530
Anton <ushkov6@mail.ru>
2023-04-28 00:28:50 +0400

    add input only numbers

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const inputEl = document.querySelector('input');
-const buttonEl = document.querySelector('button');
-const timerEl = document.querySelector('span');
+const inputEl = document.querySelector("input");
+const buttonEl = document.querySelector("button");
+const timerEl = document.querySelector("span");
 
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return (seconds) => {};
+  return seconds => {};
 };
 
 const animateTimer = createTimerAnimator();
 
-inputEl.addEventListener('input', () => {
+inputEl.addEventListener("input", e => {
   // Очистите input так, чтобы в значении
   // оставались только числа
+
+  const PATTERN = /[^0-9]/;
+
+  e.target.value = e.target.value.replace(PATTERN, "");
 });
 
-buttonEl.addEventListener('click', () => {
+buttonEl.addEventListener("click", () => {
   const seconds = Number(inputEl.value);
 
   animateTimer(seconds);
 
-  inputEl.value = '';
+  inputEl.value = "";
 });
